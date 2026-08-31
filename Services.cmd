120e38927b7ac1dbdfec3c78dcd7d45ee8556d05
0xf19 <KrypZero@Proton.ME>
2026-08-31 16:55:08 +0200

    Create Services.cmd

---
+++ Services.cmd
@@ -0,0 +1,38 @@
+sc stop DiagTrack 
+sc config DiagTrack start= disabled
+sc stop WSearch 
+sc config WSearch start= disabled
+sc stop wuauserv 
+sc config wuauserv start= disabled
+sc stop SysMain 
+sc config SysMain start= disabled
+sc stop RetailDemo
+sc config RetailDemo start= disabled
+sc stop RemoteRegisty
+sc config RetailDemo start= disabled
+sc stop Spooler
+sc config Spooler start= disabled
+sc stop WpcMonSvc
+sc config WpcMonSvc start= disabled
+sc stop InstallService
+sc config InstallService start= disabled
+sc stop edgeupdatem
+sc config edgeupdatem start= disabled
+sc stop edgeupdate
+sc config edgeupdatem start= disabled
+sc stop MicrosoftEdgeElevationService
+sc config MicrosoftEdgeElevationService start= disabled
+sc stop lfsvc
+sc config lfsvc start= disabled
+sc stop Fax
+sc config Fax start= disabled
+sc stop DoSvc
+sc config DoSvc start= disabled
+sc config XblAuthManager start= disabled
+sc config XblGameSave start= disabled
+sc config XboxNetApiSvc start= disabled
+sc stop EventLog
+sc config EventLog start= disabled
+sc config Wecsvc start= disabled
+sc config WerSvc start= disabled
+sc config WalletService start= disabled
