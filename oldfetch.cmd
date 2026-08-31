9ce38de687b62f1ca4d2817e5b8c1244e276906d
0xf19 <KrypZero@Proton.ME>
2026-08-31 18:11:51 +0200

    Create oldfetch.cmd

---
+++ oldfetch.cmd
@@ -0,0 +1,11 @@
+:: This file was on C:\Windows
+
+echo ::::::::    :::::::: 
+echo ::::::::    ::::::::
+echo ::::::::    ::::::::    
+echo ::::::::    ::::::::    OS: GarbanzoEMOS
+echo                       Release Version: 0.1
+echo ::::::::    ::::::::    WM: Desktop Window Manager
+echo ::::::::    ::::::::
+echo ::::::::    :::::::: 
+echo ::::::::    :::::::: 
